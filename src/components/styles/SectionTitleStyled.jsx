51d16486ea5d2c8812815799a3120190851ea6a0
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-12 20:32:55 -0300

    Create SectionTitleStyled component

---
+++ src/components/styles/SectionTitleStyled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const SectionTitleStyled = styled.h2`
+  font-size: 2rem;
+  text-align: center;
+  font-weight: 400;
+  text-transform: uppercase;
+  letter-spacing: 10%;
+`;
+
+export default SectionTitleStyled;
