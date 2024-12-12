8bb4ef958487d123cb6f5bb1199d46912300ce54
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-12 16:47:34 -0300

    Style header and its elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,41 @@
 import styled from "styled-components";
+import btnIcon from "../assets/images/icons/moon.svg";
 
-const Title = styled.h1`
+const HeaderStyled = styled.header`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 2rem;
+`;
+
+const TitleStyled = styled.h1`
   font-family: "Inter", sans-serif;
   font-weight: 600;
   font-size: 1rem;
   text-transform: capitalize;
 `;
 
+const ButtonStyled = styled.button`
+  border: none;
+  background: none;
+`;
+
+const IconStyled = styled.img`
+  display: block;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 function Header() {
   return (
-    <header>
-      <Title>Gabriel graciliano</Title>
-    </header>
+    <HeaderStyled>
+      <TitleStyled>Gabriel graciliano</TitleStyled>
+      <ButtonStyled>
+        <IconStyled src={btnIcon} alt="" />
+      </ButtonStyled>
+    </HeaderStyled>
   );
 }
 
