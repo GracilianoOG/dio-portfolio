7987c339c60e7f8524ba582db760288ed87002d2
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-12 20:44:08 -0300

    Add cards about courses and styling to fit them

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,15 +1,33 @@
+import styled from "styled-components";
 import EducationCard from "./EducationCard";
 import SectionTitleStyled from "./styles/SectionTitleStyled";
+
+const SectionContent = styled.div`
+  display: grid;
+  gap: 2rem;
+`;
 
 function EducationSection() {
   return (
     <section>
       <SectionTitleStyled>Formação</SectionTitleStyled>
-      <EducationCard
-        course="Análise e Desenvolvimento de Sistemas"
-        location="FAETERJ Paracambi"
-        date="março 2024 - cursando"
-      />
+      <SectionContent>
+        <EducationCard
+          course="Análise e Desenvolvimento de Sistemas"
+          location="FAETERJ Paracambi"
+          date="março 2024 - cursando"
+        />
+        <EducationCard
+          course="Técnico em Informática"
+          location="ETE Paracambi"
+          date="fev. 2021 - dez. 2021"
+        />
+        <EducationCard
+          course="Técnico em Informática para Internet"
+          location="IFRJ Paulo de Frontin"
+          date="fev. 2018 - dez. 2018"
+        />
+      </SectionContent>
     </section>
   );
 }
