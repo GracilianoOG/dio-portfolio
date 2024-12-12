a40185301026c291d259ee57ca784ca80752d368
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-12 19:53:58 -0300

    Add EducationSection
    Content has been styled to fit the multiple components that will include.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,19 @@
+import styled from "styled-components";
 import AboutSection from "./AboutSection";
+import EducationSection from "./EducationSection";
+
+const MainContentStyled = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 3.75rem;
+`;
 
 function Content() {
   return (
-    <main>
+    <MainContentStyled>
       <AboutSection />
-    </main>
+      <EducationSection />
+    </MainContentStyled>
   );
 }
 
