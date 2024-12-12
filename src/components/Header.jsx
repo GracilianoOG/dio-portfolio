fdf775745a9804ba6d7d06192e3c3302f0ae0288
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-12 16:32:08 -0300

    Style header main heading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-family: "Inter", sans-serif;
+  font-weight: 600;
+  font-size: 1rem;
+  text-transform: capitalize;
+`;
+
 function Header() {
   return (
     <header>
-      <h1>Gabriel graciliano</h1>
+      <Title>Gabriel graciliano</Title>
     </header>
   );
 }
