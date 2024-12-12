147d4acf1aa029498786ae2777a003f493f74f77
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-12 17:37:41 -0300

    Wrap main components with Wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import GlobalStyled from "./components/GlobalStyled";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <>
       <GlobalStyled />
-      <Header />
-      <Footer />
+      <Wrapper>
+        <Header />
+        <Footer />
+      </Wrapper>
     </>
   );
 }
