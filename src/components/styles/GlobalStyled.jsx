0c20091b71f17f6365acf0d2a559e78dea668729
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-13 18:31:28 -0300

    Add project CSS variables

--- src/components/styles/GlobalStyled.jsx
+++ src/components/styles/GlobalStyled.jsx
@@ -1,11 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyled = createGlobalStyle`
+  :root {
+    --color-background: #F6F6F6;
+    --color-primary: #1C1C1C;
+    --color-secondary: #FFFFFF;
+    --color-tertiary: #363636;
+    --font-family-primary: "Raleway", sans-serif;
+    --font-family-secondary: "Inter", sans-serif;
+    --gap-base: 6.25rem;
+  }
+
   *,
   *::after,
   *::before {
     box-sizing: border-box;
-    font-family: "Raleway", sans-serif;
+    font-family: var(--font-family-primary);
     margin: 0;
     padding: 0;
   }
