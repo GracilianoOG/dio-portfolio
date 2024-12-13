16487d9dfa021c8fe77c8f64cb299bce14690aa6
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-13 11:06:11 -0300

    Add image of first project

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,5 +1,6 @@
 import ProjectCard from "./ProjectCard";
 import SectionTitleStyled from "./styles/SectionTitleStyled";
+import projectOne from "../assets/images/projects/project-1.jpg";
 
 function ProjectsSection() {
   return (
@@ -7,6 +8,7 @@ function ProjectsSection() {
       <SectionTitleStyled>Projetos</SectionTitleStyled>
       <div>
         <ProjectCard
+          image={projectOne}
           projectName="3-Column preview"
           description="Component com três colunas desenvovido em React."
           featureList={[
