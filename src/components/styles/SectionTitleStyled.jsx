fc8730931ba159ab1659ebc8da54a0cedcf79d6f
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-14 12:33:47 -0300

    Add responsiveness to SectionTitleStyled

--- src/components/styles/SectionTitleStyled.jsx
+++ src/components/styles/SectionTitleStyled.jsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
 const SectionTitleStyled = styled.h2`
-  font-size: 2rem;
+  font-size: clamp(2rem, 3.6vw, 3rem);
   text-align: center;
   font-weight: 400;
   text-transform: uppercase;
   letter-spacing: 10%;
   margin-bottom: 2.5rem;
+
+  @media screen and (min-width: 64rem) {
+    margin-bottom: 3rem;
+  }
 `;
 
 export default SectionTitleStyled;
