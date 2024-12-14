980c1d7143969e92a51bf7dace6ec7438dabd7a1
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-14 10:53:25 -0300

    Add responsiveness to AboutSection

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -5,12 +5,23 @@ import linkedin from "../assets/images/icons/linkedin.svg";
 import SocialLink from "./SocialLink";
 
 const AboutSectionStyled = styled.section`
+  display: flex;
+  gap: 1.25rem;
+  flex-direction: column;
   text-align: center;
+
+  @media screen and (min-width: 64rem) {
+    flex-direction: row;
+    align-items: center;
+  }
 `;
 
 const PhotoStyled = styled.img`
-  margin-bottom: 1.25rem;
   width: 100%;
+
+  @media screen and (min-width: 64rem) {
+    max-width: 31.25rem;
+  }
 `;
 
 const AboutTitleStyled = styled.h2`
@@ -18,11 +29,19 @@ const AboutTitleStyled = styled.h2`
   font-size: 1rem;
   font-weight: 400;
   margin-bottom: 0.75rem;
+
+  @media screen and (min-width: 48rem) {
+    font-size: 2rem;
+  }
 `;
 
 const AboutTextStyled = styled.p`
   font-size: 0.75rem;
   margin-bottom: 1.5rem;
+
+  @media screen and (min-width: 48rem) {
+    font-size: 1.5rem;
+  }
 `;
 
 const ButtonWrapper = styled.div`
@@ -30,16 +49,24 @@ const ButtonWrapper = styled.div`
   justify-content: center;
 `;
 
+const ContentWrapperStyled = styled.div`
+  flex: 1;
+`;
+
 function AboutSection() {
   return (
     <AboutSectionStyled id="about">
       <PhotoStyled src={photo} alt="" />
-      <AboutTitleStyled>Olá, eu sou o Gabriel, muito prazer!</AboutTitleStyled>
-      <AboutTextStyled>Apaixonado por tecnologia e inovação.</AboutTextStyled>
-      <ButtonWrapper>
-        <SocialLink imageSrc={githubIcon} href="#" />
-        <SocialLink imageSrc={linkedin} href="#" />
-      </ButtonWrapper>
+      <ContentWrapperStyled>
+        <AboutTitleStyled>
+          Olá, eu sou o Gabriel, muito prazer!
+        </AboutTitleStyled>
+        <AboutTextStyled>Apaixonado por tecnologia e inovação.</AboutTextStyled>
+        <ButtonWrapper>
+          <SocialLink imageSrc={githubIcon} href="#" />
+          <SocialLink imageSrc={linkedin} href="#" />
+        </ButtonWrapper>
+      </ContentWrapperStyled>
     </AboutSectionStyled>
   );
 }
