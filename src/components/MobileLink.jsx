565b1f535d68b3cd61f1a1b7113a68172bdccda5
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-15 17:56:20 -0300

    Create MobileLink component

---
+++ src/components/MobileLink.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const LinkWrapperStyled = styled.a`
+  color: var(--color-primary);
+  font-size: 0.75rem;
+  font-weight: 600;
+  padding: 0.625rem 0;
+  text-align: center;
+  text-transform: uppercase;
+
+  & img {
+    margin-bottom: 0.188rem;
+  }
+`;
+
+function MobileLink({ imageSrc, children, href }) {
+  return (
+    <LinkWrapperStyled href={href}>
+      <img src={imageSrc} alt="" />
+      <p>{children}</p>
+    </LinkWrapperStyled>
+  );
+}
+
+MobileLink.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+};
+
+export default MobileLink;
