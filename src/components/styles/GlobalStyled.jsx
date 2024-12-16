3183ff081a561ecb84a59ea36d9f1a986b744ce7
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-15 22:03:15 -0300

    Add dark theme style

--- src/components/styles/GlobalStyled.jsx
+++ src/components/styles/GlobalStyled.jsx
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
 import { devices } from "../../utils/breakpoints";
 
 const GlobalStyled = createGlobalStyle`
@@ -8,6 +8,19 @@ const GlobalStyled = createGlobalStyle`
     --color-secondary: #FFFFFF;
     --color-tertiary: #363636;
     --color-mobile-line: #b6b6b6;
+    --icon-filter: none;
+    
+    ${props =>
+      props.$isDarkMode &&
+      css`
+        --color-background: #1c1c1c;
+        --color-primary: #ffffff;
+        --color-secondary: #1c1c1c;
+        --color-tertiary: #9a9a9c;
+        --color-mobile-line: #b6b6b6;
+        --icon-filter: invert(1);
+      `}
+      
     --font-family-primary: "Raleway", sans-serif;
     --font-family-secondary: "Inter", sans-serif;
     --gap-base: 6.25rem;
