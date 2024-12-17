dea145ab6e20a745c5caef8f09af18e1c8a017df
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-16 23:07:27 -0300

    Add hover effect on IconButton

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -4,10 +4,15 @@ import IconStyled from "./styles/IconStyled";
 
 const ButtonStyled = styled.button`
   border: none;
-  background: none;
+  background-color: transparent;
+  border-radius: 50%;
+  padding: 0.5rem;
+  transition: transform 0.5s, filter 0.5s;
 
   &:hover {
     cursor: pointer;
+    transform: scale(1.1);
+    filter: drop-shadow(0 0 0.625rem var(--color-primary)) brightness(1.5);
   }
 `;
 
