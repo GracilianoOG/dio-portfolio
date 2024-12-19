639e4bf1ef745873b24a84143d456ef20da21d53
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-19 17:40:42 -0300

    Refactor GlobalStyled into smaller chunks of code

--- src/components/styles/GlobalStyled.jsx
+++ src/components/styles/GlobalStyled.jsx
@@ -1,35 +1,7 @@
 import { createGlobalStyle, css } from "styled-components";
 import { devices } from "../../utils/breakpoints";
 
-const GlobalStyled = createGlobalStyle`
-  :root {
-    --color-background: #F6F6F6;
-    --color-primary: #1C1C1C;
-    --color-secondary: #FFFFFF;
-    --color-tertiary: #363636;
-    --color-mobile-line: #b6b6b6;
-    --icon-filter: none;
-    
-    ${props =>
-      props.$isDarkMode &&
-      css`
-        --color-background: #1c1c1c;
-        --color-primary: #ffffff;
-        --color-secondary: #1c1c1c;
-        --color-tertiary: #9a9a9c;
-        --color-mobile-line: #b6b6b6;
-        --icon-filter: invert(1);
-      `}
-      
-    --font-family-primary: "Raleway", sans-serif;
-    --font-family-secondary: "Inter", sans-serif;
-    --gap-base: 6.25rem;
-
-    @media screen and ${devices.md} {
-      --gap-base: 12.5rem;
-    }
-  }
-
+const resetCSS = css`
   *,
   *::after,
   *::before {
@@ -38,7 +10,9 @@ const GlobalStyled = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+`;
 
+const globalCSS = css`
   html {
     scroll-behavior: smooth;
     scroll-padding-top: 7.5rem;
@@ -52,6 +26,42 @@ const GlobalStyled = createGlobalStyle`
   a {
     text-decoration: none;
   }
+`;
+
+const variablesCSS = css`
+  :root {
+    --color-background: #f6f6f6;
+    --color-primary: #1c1c1c;
+    --color-secondary: #ffffff;
+    --color-tertiary: #363636;
+    --color-mobile-line: #b6b6b6;
+    --icon-filter: none;
+
+    ${props =>
+      props.$isDarkMode &&
+      css`
+        --color-background: #1c1c1c;
+        --color-primary: #ffffff;
+        --color-secondary: #1c1c1c;
+        --color-tertiary: #9a9a9c;
+        --color-mobile-line: #b6b6b6;
+        --icon-filter: invert(1);
+      `}
+
+    --font-family-primary: "Raleway", sans-serif;
+    --font-family-secondary: "Inter", sans-serif;
+    --gap-base: 6.25rem;
+
+    @media screen and ${devices.md} {
+      --gap-base: 12.5rem;
+    }
+  }
+`;
+
+const GlobalStyled = createGlobalStyle`
+  ${variablesCSS}
+  ${resetCSS}
+  ${globalCSS}
 `;
 
 export default GlobalStyled;
