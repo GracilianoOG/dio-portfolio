c1f39fe780d4ca9ae5cbdabb491dadaa75586dc8
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-20 10:48:45 -0300

    Replace list items with MenuItem on DesktopMenu

--- src/components/DesktopMenu.jsx
+++ src/components/DesktopMenu.jsx
@@ -1,51 +1,19 @@
 import styled from "styled-components";
+import MenuItem from "./MenuItem";
 
 const ListStyled = styled.ul`
   display: flex;
   list-style: none;
-  text-transform: uppercase;
-`;
-
-const LinkStyled = styled.a`
-  color: var(--color-primary);
-  padding: 0.313rem;
-  position: relative;
-
-  &::before {
-    --line-scale: 0;
-    background-color: var(--color-primary);
-    bottom: 0;
-    content: "";
-    position: absolute;
-    left: 0;
-    height: 0.125rem;
-    transition: transform 0.4s;
-    transform: scaleX(var(--line-scale));
-    transform-origin: center;
-    width: 100%;
-  }
-
-  &:hover::before {
-    --line-scale: 1;
-  }
 `;
 
 function DesktopMenu() {
   return (
     <nav>
       <ListStyled>
-        <li>
-          <LinkStyled href="#top">Home</LinkStyled>
-        </li>
-        <li>
-          <LinkStyled href="#education">Formação</LinkStyled>
-        </li>
-        <li>
-          <LinkStyled href="#projects">Projetos</LinkStyled>
-        </li>
-        <li>
-          <LinkStyled href="#contact">Contato</LinkStyled>
-        </li>
+        <MenuItem href="#top">Home</MenuItem>
+        <MenuItem href="#education">Formação</MenuItem>
+        <MenuItem href="#projects">Projetos</MenuItem>
+        <MenuItem href="#contact">Contato</MenuItem>
       </ListStyled>
     </nav>
   );
