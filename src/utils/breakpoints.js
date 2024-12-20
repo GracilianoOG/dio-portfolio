c02339d47e25a01340cd1a3dddf55c4e0cb14721
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-20 13:27:03 -0300

    Fix broken menu layout at around 1023px

--- src/utils/breakpoints.js
+++ src/utils/breakpoints.js
@@ -1,10 +1,11 @@
 const size = {
   md: "48rem",
   lg: "64rem",
+  mobileOnly: "63.988rem",
 };
 
 export const devices = {
   md: `(min-width: ${size.md})`,
   lg: `(min-width: ${size.lg})`,
-  mobileOnly: `(max-width: ${size.lg})`,
+  mobileOnly: `(max-width: ${size.mobileOnly})`,
 };
