0d8b6bc96b0ac625587534f73c51d4e47704616a
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-22 18:45:02 -0300

    Check theme preference

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import Content from "./components/Content";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [darkState, setDarkState] = useState(false);
+
+  useEffect(() => {
+    const isDarkEnabled =
+      JSON.parse(localStorage.getItem("dark-theme")) ??
+      matchMedia("(prefers-color-scheme: dark)").matches;
+
+    setDarkState(isDarkEnabled);
+  }, []);
 
   return (
     <>
