c7bdc700af377ec92929e2f87278c6b23b5bf3e9
Gabriel <gabriel.gracilianomb@gmail.com>
2024-12-23 12:46:30 -0300

    Use new theme hook to handle dark state

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,10 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import Content from "./components/Content";
-import { useEffect, useState } from "react";
+import useThemePreference from "./hooks/useThemePreference";
 
 function App() {
-  const [darkState, setDarkState] = useState(false);
-
-  useEffect(() => {
-    const isDarkEnabled =
-      JSON.parse(localStorage.getItem("dark-theme")) ??
-      matchMedia("(prefers-color-scheme: dark)").matches;
-
-    setDarkState(isDarkEnabled);
-  }, []);
+  const [darkState, setDarkState] = useThemePreference();
 
   return (
     <>
