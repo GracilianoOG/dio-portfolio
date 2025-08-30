c297ac8a404c08556f3a9c214a023e63fbb0cd64
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-30 16:26:14 -0300

    Limit max size on bigger screens

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -4,13 +4,14 @@ import { devices } from "../utils/breakpoints";
 
 const WrapperStyled = styled.div`
   margin: 1rem 1rem 7.5rem 1rem;
+  max-width: 90rem;
 
   @media screen and ${devices.md} {
     margin: 2.5rem 2.5rem 8rem 2.5rem;
   }
 
   @media screen and ${devices.lg} {
-    margin: 4rem;
+    margin: 4rem auto;
   }
 `;
 
