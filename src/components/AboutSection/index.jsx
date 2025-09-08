2ac04162a110c35bd4b1989570e73cd6fbd6ddb5
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-08 14:30:57 -0300

    Extract AboutSection to a folder

---
+++ src/components/AboutSection/index.jsx
@@ -0,0 +1,42 @@
+import photo from "../../assets/images/photo.jpg";
+import githubIcon from "../../assets/images/icons/github.svg";
+import linkedin from "../../assets/images/icons/linkedin.svg";
+import SocialLink from "../SocialLink";
+import {
+  AboutSectionStyled,
+  AboutTextStyled,
+  AboutTitleStyled,
+  ButtonWrapper,
+  PhotoStyled,
+} from "./styles";
+
+function AboutSection() {
+  return (
+    <AboutSectionStyled id="about">
+      <div>
+        <AboutTitleStyled>
+          Olá, eu sou o Gabriel, muito prazer!
+        </AboutTitleStyled>
+        <AboutTextStyled>Apaixonado por tecnologia e inovação.</AboutTextStyled>
+        <ButtonWrapper>
+          <SocialLink
+            imageSrc={githubIcon}
+            href="https://github.com/GracilianoOG"
+            text="GitHub"
+          />
+          <SocialLink
+            imageSrc={linkedin}
+            href="https://www.linkedin.com/in/gabrielgmbarros"
+            text="LinkedIn"
+          />
+        </ButtonWrapper>
+      </div>
+      <PhotoStyled
+        src={photo}
+        alt="Jovem branco na casa dos 20 anos, desenvolvedor do site, sorrindo para a câmera enquanto está em uma praia."
+      />
+    </AboutSectionStyled>
+  );
+}
+
+export default AboutSection;
---
+++ src/components/AboutSection/styles.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { devices } from "../../utils/breakpoints";
+
+export const AboutSectionStyled = styled.section`
+  align-items: center;
+  display: flex;
+  gap: 1.25rem;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  @media screen and ${devices.lg} {
+    flex-direction: row;
+  }
+`;
+
+export const PhotoStyled = styled.img`
+  border-radius: 50%;
+  max-width: 25rem;
+  width: 100%;
+
+  @media screen and ${devices.lg} {
+    max-width: min(31.25rem, 38.11%);
+  }
+`;
+
+export const AboutTitleStyled = styled.h2`
+  font-family: var(--font-family-secondary);
+  font-size: clamp(1rem, 5vw, 5rem);
+  font-weight: 400;
+  margin-bottom: 0.75rem;
+`;
+
+export const AboutTextStyled = styled.p`
+  font-size: clamp(0.75rem, 1.75vw, 1.5rem);
+  font-style: italic;
+  margin-bottom: 1.5rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+`;
