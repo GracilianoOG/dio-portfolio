502182feb3282938cc3b545cee6658403514dff7
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-08 14:45:17 -0300

    Move styled components to styles

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,15 +1,8 @@
-import styled from "styled-components";
 import AboutSection from "../AboutSection";
 import EducationSection from "../EducationSection";
 import ProjectsSection from "../ProjectsSection";
 import ContactSection from "../ContactSection";
-
-const MainContentStyled = styled.main`
-  display: flex;
-  flex-direction: column;
-  gap: var(--gap-base);
-  margin-bottom: var(--gap-base);
-`;
+import { MainContentStyled } from "./styles";
 
 function Content() {
   return (
---
+++ src/components/Content/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const MainContentStyled = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: var(--gap-base);
+  margin-bottom: var(--gap-base);
+`;
