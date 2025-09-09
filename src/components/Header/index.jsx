47859a838e23a20a060539b32a6d76497fabf3ea
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-08 22:49:37 -0300

    Remove props from Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,15 @@
 import ThemeSwitcher from "../ThemeSwitcher";
-import PropTypes from "prop-types";
 import { HeaderStyled, TitleStyled } from "./styles";
 import NavMenu from "../NavMenu";
 
-function Header(props) {
+function Header() {
   return (
     <HeaderStyled>
       <TitleStyled>Gabriel graciliano</TitleStyled>
       <NavMenu />
-      <ThemeSwitcher {...props} />
+      <ThemeSwitcher />
     </HeaderStyled>
   );
 }
-
-Header.propTypes = {
-  setDarkState: PropTypes.func.isRequired,
-  darkState: PropTypes.bool.isRequired,
-};
 
 export default Header;
