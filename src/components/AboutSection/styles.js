7fe2250d7e4705717b9608594dadbef8b22feefa
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-30 10:30:00 -0300

    Increase title font weight

--- src/components/AboutSection/styles.js
+++ src/components/AboutSection/styles.js
@@ -26,7 +26,7 @@ export const PhotoStyled = styled.img`
 export const AboutTitleStyled = styled.h2`
   font-family: var(--font-family-secondary);
   font-size: clamp(1rem, 5vw, 5rem);
-  font-weight: 400;
+  font-weight: 500;
   margin-bottom: 0.75rem;
 `;
 
